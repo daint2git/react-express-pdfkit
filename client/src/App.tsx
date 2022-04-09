697e8673199d8e90b3932b8942981c41e7af2d82
daint-1849 <nguyen.tran.dai@sun-asterisk.com>
2022-04-09 18:58:16 +0700

    update code

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,11 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import "./App.css";
 import logo from "./logo.svg";
 
 function App() {
   const [filename, setFilename] = useState<null | string>(null);
+  const abortControllerRef = useRef<AbortController | null>(null);
+
   const generatePdf = async () => {
     const response = await fetch("/api/generate-pdf");
     const json: { filename: string } = await response.json();
@@ -41,6 +43,77 @@ function App() {
     }
   };
 
+  const downloadPdfProgress = async () => {
+    abortControllerRef.current = new AbortController();
+    abortControllerRef.current.signal.addEventListener("abort", (data) => {
+      console.log("abort", data);
+    });
+
+    const response = await fetch("/api/download-pdf-progress", {
+      signal: abortControllerRef.current.signal,
+    });
+    const percent = document.getElementById(
+      "downloadedPercent"
+    ) as HTMLProgressElement;
+
+    if (!percent) return;
+
+    percent.value = 0;
+    percent.innerHTML = "0%";
+
+    if (!response.body) return;
+    const reader = response.body.getReader();
+    const contentLength = response.headers.get("Content-Length");
+    const contentType = response.headers.get("content-type");
+    const contentDisposition = response.headers.get("content-disposition");
+
+    if (!contentLength || !contentType || !contentDisposition) return;
+
+    const filename = contentDisposition.substring(
+      contentDisposition.indexOf('"') + 1,
+      contentDisposition.lastIndexOf('"')
+    );
+
+    let receivedLength = 0; // received that many bytes at the moment
+    const chunks = []; // array of received binary chunks (comprises the body)
+
+    while (true) {
+      const { done, value } = await reader.read();
+
+      if (done) {
+        break;
+      }
+
+      if (!value) {
+        break;
+      }
+
+      chunks.push(value);
+      receivedLength += value.length;
+
+      console.log(`Received ${receivedLength} of ${contentLength}`);
+
+      const nextPercent = Math.floor((receivedLength / +contentLength) * 100);
+      console.log("nextPercent", nextPercent);
+
+      percent.value = nextPercent;
+      percent.innerHTML = nextPercent + "%";
+    }
+
+    const blob = new Blob(chunks);
+
+    const link = document.createElement("a");
+    link.download = filename;
+    link.href = URL.createObjectURL(blob);
+    link.click();
+
+    URL.revokeObjectURL(link.href);
+  };
+
+  const cancelDownloadPdfProgress = () => {
+    abortControllerRef.current?.abort();
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -55,7 +128,17 @@ function App() {
           </a>
         )}
         <div></div>
+        <br />
         <button onClick={downloadPdfDirectly}>download pdf directly</button>
+        <div></div>
+        <br />
+        <button onClick={downloadPdfProgress}>download pdf progress</button>
+        <br />
+        <progress id="downloadedPercent" value="0" max="100">
+          0
+        </progress>
+        <br />
+        <button onClick={cancelDownloadPdfProgress}>cancel download</button>
       </div>
     </div>
   );
--- server/src/main.ts
+++ server/src/main.ts
@@ -70,6 +70,95 @@ app.get("/api/generate-pdf", (req, res) => {
   });
 });
 
+app.get("/api/download-pdf-progress", (_req, res) => {
+  const doc = new PDFDocument();
+
+  // Add an image, constrain it to a given size, and center it vertically and horizontally
+  const image = path.resolve(process.cwd(), "assets/flower.jpeg");
+  doc.image(image, {
+    fit: [250, 300],
+    align: "center",
+    valign: "center",
+  });
+  doc.image(image, {
+    fit: [300, 400],
+    align: "center",
+    valign: "center",
+  });
+  doc.image(image, {
+    fit: [400, 500],
+    align: "center",
+    valign: "center",
+  });
+  doc.image(image, {
+    fit: [500, 600],
+    align: "center",
+    valign: "center",
+  });
+
+  doc.addPage().fontSize(25).text("Here is some vector graphics...", 100, 100);
+  doc.addPage().fontSize(25).text("Here is some vector graphics...", 100, 100);
+  doc.addPage().fontSize(25).text("Here is some vector graphics...", 100, 100);
+  doc.addPage().fontSize(25).text("Here is some vector graphics...", 100, 100);
+
+  // Draw a triangle
+  doc.save().moveTo(100, 150).lineTo(100, 250).lineTo(200, 250).fill("#FF3300");
+
+  // Apply some transforms and render an SVG path with the 'even-odd' fill rule
+  doc
+    .scale(0.6)
+    .translate(470, -380)
+    .path("M 250,75 L 323,301 131,161 369,161 177,301 z")
+    .fill("red", "even-odd")
+    .restore();
+
+  // Add some text with annotations
+  for (let index = 0; index < 3000; index++) {
+    doc
+      .fillColor("blue")
+      .text("Here is a link!", 100, 100)
+      .underline(100, 100, 160, 27, { color: "#0000FF" })
+      .link(100, 100, 160, 27, "http://google.com/");
+  }
+
+  let buffers: Buffer[] = [];
+
+  doc.on("data", buffers.push.bind(buffers));
+
+  doc.on("end", () => {
+    let pdfData = Buffer.concat(buffers);
+
+    console.log("pdfData.length", pdfData.length);
+
+    res.writeHead(200, {
+      "Content-Length": pdfData.length,
+      "Content-Type": "application/pdf",
+      "Content-disposition": 'attachment;filename="download-pdf-progress.pdf"',
+    });
+
+    // res.writeHead(200, {
+    //   "Transfer-Encoding": "chunked",
+    // });
+
+    const chunks = [];
+    let i = 0;
+    const max = pdfData.length;
+
+    while (i < max) {
+      chunks.push(pdfData.slice(i, (i += 1000)));
+    }
+
+    for (const chunk of chunks) {
+      res.write(chunk);
+    }
+
+    res.end();
+  });
+
+  // Finalize PDF file
+  doc.end();
+});
+
 app.get("/api/download-file/:filename", (req, res) => {
   const { filename } = req.params;
   const outputPath = path.resolve(process.cwd(), "output");
